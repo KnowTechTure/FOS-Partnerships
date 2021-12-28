225693554a2c9594b8b98e67154d2f80864dde14
Joaquin Urbano Serra <kimbalear@gmail.com>
2021-12-28 17:44:00 +0100

    LYT2
    windowsize
    show-hide ui_nav

--- script_ui/ui_main.js
+++ script_ui/ui_main.js
@@ -1,5 +1,61 @@
 $(document).ready(function () {
 
+    var init = 2;
+
+
+    $(window).resize(function() {
+        var windowsize = $(window).width();
+
+        $('.LYT2-Wrapper').css({
+            "width": windowsize
+        });
+    });
+
+    // show - hide ui_nav
+    $(document).bind('keypress', function (event) {
+        var w100 = $(window).width();
+        var wp_200 = w100 - 200;
+
+        if (event.which === 83 && event.shiftKey) {
+            // SHIFT + S
+            $('.grd_nav').show();
+            $('.grd_layout').css({
+                "width": wp_200
+            });
+
+            $('.LYT2-Wrapper').css({
+                "width": wp_200
+            });
+
+        };
+        if (event.which === 72 && event.shiftKey) {
+            // SHIFT + H
+            $('.grd_nav').hide();
+            $('.grd_layout').css({
+                "width": w100
+            });
+
+            $('.LYT2-Wrapper').css({
+                "width": w100
+            });
+        }
+    });
+
+    switch (init) {
+        case 1:
+            $('.LYT2-Wrapper').hide();
+            $('.LYT1-Wrapper').show();
+            break;
+        case 2:
+            $('.LYT1-Wrapper').hide();
+            $('.LYT2-Wrapper').show();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+
     $("#partner").click(function () {
         $('#fos_cnt').hide();
         $('#partner_cnt').show();
@@ -22,7 +78,7 @@ $(document).ready(function () {
         $('#fos_cnt').show();
         $("#partner").removeClass('active_nav');
         $("#fos").addClass('active_nav');
-        
+
     });
-    
+
 });
