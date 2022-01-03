d092998e21a0029faeb0ebbf32fdd386bcf92b68
Joaquin Urbano Serra <kimbalear@gmail.com>
2022-01-03 16:17:14 +0100

    lyt4

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,21 +1,11 @@
 $(document).ready(function () {
 	$('#tabs li').click(function () {
 		id = $(this).attr('id');
-/*
-		switch (id) {
-			case "chat":
-				break;
-			case "tasks":
-				break;
-		}
-*/
-		//alert(id);
-		$('#'+id).addClass("active");
-		$('li:not(#' + id +')').removeClass("active");
-		$('#cont-' + id).show();
-		$('#cont div:not(#cont-' + id +')').hide();
+		
+		$('#tabs #'+id).addClass("active");
+		$('#tabs li:not(#' + id +')').removeClass("active");
 
-		//$('#cont div#js-' + id).animate({width: 'toggle'});
-		//$('#cont div:not(#js-' + id + ')').animate({width: 'toggle'});
+		$('#cont #cont-' + id).show();
+		$('#cont div:not(#cont-' + id +')').hide();
 	});
 });
